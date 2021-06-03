66650e49e361663681a474f0c97bd24c9e50eb53
Kushaan Gowda <kushaangowda@yahoo.com>
2021-06-03 14:06:55 +0530

    student router

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,7 +1,49 @@
 const router = require("express").Router();
+const Student = require("../models/student.model");
 
 router.route("/").get((req, res) => {
-	res.send("Student");
+	Student.find()
+		.then((students) => res.json({ success: "Students fetched successfully", students }))
+		.catch((err) => res.json({ failure: "Unable to fetch students", error: err }));
+});
+
+router.route("/addOne").post((req, res) => {
+	const data = {
+		email: req.body.email,
+		branch: req.body.branch,
+		year: Number(req.body.year),
+	};
+
+	const newStudent = new Student(data);
+
+	newStudent
+		.save()
+		.then(() => res.json({ success: "Student added successfully" }))
+		.catch((err) => res.json({ failure: "Unable to add student", error: err }));
+});
+
+router.route("/addMany").post((req, res) => {
+	Student.insertMany(req.body.data)
+		.then(() => res.json({ success: "Students added successfully" }))
+		.catch((err) => res.json({ failure: "Unable to add students", error: err }));
+});
+
+router.route("/deleteAll").delete((req, res) => {
+	Student.deleteMany()
+		.then(() => res.json({ success: "All Students' data deleted successfully" }))
+		.catch((err) => res.json({ failure: "Unable to delete all students' data", error: err }));
+});
+
+router.route("/addNotification/:id").post((req, res) => {
+	Student.findById(req.params.id)
+		.then((student) => {
+			student.notifications = [req.body.post, ...student.notifications];
+			student
+				.save()
+				.then(() => res.json({ success: "Notification added successfully" }))
+				.catch((err) => res.json({ failure: "Unable to add notification", error: err }));
+		})
+		.catch((err) => res.json({ failure: "Unable to find student", error: err }));
 });
 
 module.exports = router;
