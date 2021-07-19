1fb96c3f31cc39f234339331d5bafcfcc058c768
Akash Gupta <agbichpuriya@gmail.com>
2021-07-19 13:41:38 +0530

    added schema_version

--- models/student.model.js
+++ models/student.model.js
@@ -7,6 +7,7 @@ const studentSchema = new Schema(
 		branch: { type: String, required: true },
 		year: { type: Number, required: true, default: 1 },
 		notifications: { type: [{ text: { type: String, default: "" }, subject: { type: String, default: "" }, companyName: { type: String, default: "" }, date:{type: Date, default: Date.now } }], required: true, default: [] },
+		schema_version: 2
 	},
 	{
 		timestamps: true,
